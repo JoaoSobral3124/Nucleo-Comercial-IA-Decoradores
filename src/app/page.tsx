f4bfa1dd221dac31d33a2768e1c5ee0ae1201df1
Condexpress <jpsilvasobral@gmail.com>
2026-04-13 00:14:26 -0300

    page atualização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 
 export default function Home() {
     return(
+        
         <main>
         <h1>Página Home</h1> 
-        <span>Seja bem vindo a páginma home</span>
+        <span>Seja bem vindo a página home</span>
         </main>
     )
 }
