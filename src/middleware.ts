cf0e6d680cece1a2ac180723e259c48c791be2fd
João Sobral <jpsilvasobral@gmail.com>
2026-05-19 19:33:39 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,13 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  // 1. Cria uma resposta inicial
   let response = NextResponse.next({
     request: {
       headers: request.headers,
     },
   })
 
-  
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -19,21 +17,16 @@ export async function middleware(request: NextRequest) {
           return request.cookies.getAll()
         },
         setAll(cookiesToSet) {
-
           cookiesToSet.forEach(({ name, value, options }) =>
-            request.cookies.set(name, value)
+            request.cookies.set({ name, value, ...options })
           )
-          
-          // Atualiza a RESPOSTA (para que o navegador salve os novos cookies)
-          // Em vez de recriar a resposta, apenas modificamos a existente
           response = NextResponse.next({
             request: {
               headers: request.headers,
             },
           })
-          
           cookiesToSet.forEach(({ name, value, options }) =>
-            response.cookies.set(name, value, options)
+            response.cookies.set({ name, value, ...options })
           )
         },
       },
@@ -47,7 +40,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
