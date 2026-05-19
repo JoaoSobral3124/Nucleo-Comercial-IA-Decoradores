654fbb53476a61c28fffdf04848a026ffcba6af6
João Sobral <jpsilvasobral@gmail.com>
2026-05-19 20:03:46 -0300

    Update layout.tsx

--- src/app/Login/layout.tsx
+++ src/app/Login/layout.tsx
@@ -1,23 +1,11 @@
-import { Inter } from 'next/font/google'
-import './globals.css' // Importação dos estilos globais do Tailwind
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Núcleo Comercial IA',
-  description: 'Plataforma Inteligente',
+interface LoginLayoutProps {
+  children: React.ReactNode
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function LoginLayout({ children }: LoginLayoutProps) {
   return (
-    <html lang="pt-br" className="dark" suppressHydrationWarning>
-      <body className={`${inter.className} bg-black antialiased`}>
-        {children}
-      </body>
-    </html>
+    <>
+      {children}
+    </>
   )
 }
